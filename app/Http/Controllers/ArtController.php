b9fe62cadb40b81bcbb0ea0877ed4d4424d740e2
Raul-MA <a00397860@itesm.mx>
2015-11-25 11:36:47 -0600

    Art Controller views

--- app/Http/Controllers/ArtController.php
+++ app/Http/Controllers/ArtController.php
@@ -16,7 +16,7 @@ class ArtController extends Controller
      */
     public function index()
     {
-        //
+        return view('art.index');
     }
 
     /**
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -24,7 +24,7 @@ Route::get('/', function () {
 });
 
 Route::get('art',function(){
-	return view('art');
+	return view('art.index');
 });
 
 /*
